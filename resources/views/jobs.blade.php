c9428410ee87cfff97a5c207e77f18df683b395d
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-23 20:51:16 -0300

    refactor: display jobs with a listing and link style

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,7 +3,18 @@
         Job Listing
     </x-slot:heading>
 
-    @foreach ($jobs as $job)
-        <li><strong>{{$job['title']}}:</strong> Pays{{$job['salary']}} per year</li>
-    @endforeach
+    <ul>
+        @foreach ($jobs as $job)
+            <li>
+                <a href="/jobs/{{$job['id']}}">
+                    <strong>{{$job['title']}}:</strong>
+                    <span class="text-blue-500 hover:underline">Pays
+                        {{$job['salary']}}
+                        per year
+                    </span>
+                </a>
+            </li>
+        @endforeach
+    </ul>
+
 </x-layout>
