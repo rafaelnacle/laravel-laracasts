2740f6dda9c2f436fcccacb183cb2dfd91668688
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-05-28 22:11:47 -0300

    Removed tag references

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,6 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'admin' => false
         ];
     }
 
@@ -43,11 +42,4 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
-
-    public function admin(): static
-    {
-        return $this->state(fn(array $attributes) => [
-            'admin' => true,
-        ]);
-    }// User::factory()->admin()->create();
 }
