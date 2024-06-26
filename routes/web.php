7d48e56e2e0b28265a6cbb41093444e844fc120c
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2024-06-26 18:48:01 -0300

    refactor: use route resource

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+// Example of what I had.
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
+
+// This way we can control which route we want as well
+// Route::resource('jobs', JobController::class, [
+//     'only' =>
+//     'except' => ['edit']
+// ]);
+
+// This makes all of the routes for a typical REST api
+Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
